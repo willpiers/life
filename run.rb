f4ce9fb1e255806ec78a8cba88829d0f25dd7f6a
Will Piers <wpiers0405@gmail.com>
2014-06-12 09:49:25 -0600

    wrote a little script to open a file with the original configuration and print an animation of the first 1000 time steps.

--- run.rb
+++ run.rb
@@ -1,5 +1,29 @@
 require_relative './lib/map.rb'
+require 'colorize'
 
-m = Map.new "0 1 0 0 0\n1 0 0 1 1\n1 1 0 0 1\n0 1 0 0 0\n1 0 0 0 1"
-puts m.next_map.inspect
+rows = []
+matrix = File.expand_path('./starting_matrix.txt', File.dirname(__FILE__))
+File.open matrix, 'r' do |file|
+	while line = file.gets
+		rows << line.split('').map { |val| val.to_i }
+	end
+end
 
+m = Matrix.rows(rows)
+starting_map = Map.new m
+
+1000.times do
+	system 'clear'
+	starting_map.next_map.to_a.each do |row|
+		printable_row = row.map do |val|
+			if val == 1
+				"#{val} ".colorize(color: :light_yellow, background: :light_yellow)
+			else
+				"#{val} ".colorize(color: :black, background: :black)
+			end
+		end
+		puts printable_row.join
+	end
+	starting_map = Map.new starting_map.next_map
+	sleep 0.05
+end
